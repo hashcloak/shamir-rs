2647ea73a0bc8d7a5ee7edd2a1fc71acb65546b2
hdvanegasm <hdvanegasm@gmail.com>
2023-11-30 20:56:53 -0500

    Using the connections that are already established. This doesn't work.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 mod shamir_secret_sharing;
 
-use shamir_secret_sharing::{get_shares_secret, generate_secret};
 use shamir_secret_sharing::Fq;
+use shamir_secret_sharing::{generate_secret, get_shares_secret};
 use std::collections::HashMap;
-use std::env;
 use std::sync::Arc;
-use tokio::net::{TcpListener, TcpStream};
+use std::{clone, env};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::Mutex;
 use tokio::time::{self, Duration};
 
@@ -20,10 +20,7 @@ async fn send_to_port(port: u16, message: String) {
     }
 }
 
-fn get_shares_for_parties(
-  secret: Fq,
-  current_id: u64, 
-  other_parties: [u64;2]) -> Vec<(Fq,Fq)> {
+fn get_shares_for_parties(secret: Fq, current_id: u64, other_parties: [u64; 2]) -> Vec<(Fq, Fq)> {
     let mut inputs = Vec::new();
     inputs.push(current_id);
     inputs.push(other_parties[0]);
@@ -33,11 +30,12 @@ fn get_shares_for_parties(
 
 // Handeling of incoming commands for MPC party
 async fn mpc_party(
-  mut incoming_connection: TcpStream, 
-  secret: Fq, 
-  current_id: u64, 
-  other_parties: [u64;2],
-  shares_storage: Arc<Mutex<Vec<(u64, Fq, Fq)>>>
+    mut incoming_connection: TcpStream,
+    out_connections: Arc<Mutex<HashMap<String, TcpStream>>>,
+    secret: Fq,
+    current_id: u64,
+    other_parties: [u64; 2],
+    shares_storage: Arc<Mutex<Vec<(u64, Fq, Fq)>>>,
 ) {
     let mut buf = [0; 1024];
     let n = incoming_connection.read(&mut buf).await.unwrap();
@@ -50,76 +48,121 @@ async fn mpc_party(
             // Generate n shares, keep 1 and send n-1 to other parties
             println!("Secret is {}", secret);
             let mut shares = get_shares_for_parties(secret, current_id, other_parties);
-            
+
             println!("Shares are {:#?}", shares);
             // Send shares to other ports
             for (index, &target_port) in target_ports.iter().enumerate() {
                 if let Ok(target_port) = target_port.parse::<u16>() {
-                  // TODO get the right share for the right port.
+                    // TODO get the right share for the right port.
                     let share = shares.remove(index);
-                    println!("sending RECEIVE_SHARE {} {}", share.0, share.1);
-                    send_to_port(target_port, format!("RECEIVE_SHARE {} {} {}", current_id, share.0, share.1)).await;
+                    println!(
+                        "sending RECEIVE_SHARE {} {} to port {}",
+                        share.0, share.1, target_port
+                    );
+
+                    let mut connections_lock = out_connections.lock().await;
+                    println!("The hashmap is {:?}", connections_lock);
+                    let out_stream = connections_lock.get_mut(&target_port.to_string()).unwrap();
+
+                    out_stream.writable().await.unwrap();
+                    println!("Remote address: {:?}", out_stream.peer_addr());
+                    let result_writing = out_stream
+                        .write(
+                            format!("RECEIVE_SHARE {} {} {}", current_id, share.0, share.1)
+                                .as_bytes(),
+                        )
+                        .await;
+
+                    match result_writing {
+                        Ok(size) => {
+                            println!("Size of sent: {}", size);
+                        }
+                        Err(msg) => {
+                            println!("Error writing to stream: {}", msg);
+                        }
+                    }
+
+                    // send_to_port(target_port, format!("RECEIVE_SHARE {} {} {}", current_id, share.0, share.1)).await;
                 }
             }
             // Store own share, which is the last share that's left over
             let own_share = shares.remove(0);
             // Wrong port id, but we're going to substitute this all for id's anyway
-            shares_storage.lock().await.push((current_id, own_share.0, own_share.1));
-        },
+            shares_storage
+                .lock()
+                .await
+                .push((current_id, own_share.0, own_share.1));
+        }
         // Handle an incoming share of another party
         ["RECEIVE_SHARE", id_party, x, y] => {
-            println!("Received at party {} from party {}: SHARE {} {}", current_id.to_string(), id_party, x, y);
+            println!(
+                "Received at party {} from party {}: SHARE {} {}",
+                current_id.to_string(),
+                id_party,
+                x,
+                y
+            );
             let received_id_party: u64 = id_party.parse().unwrap();
             let received_x: Fq = x.parse().unwrap();
             let received_y: Fq = y.parse().unwrap();
-            shares_storage.lock().await.push((received_id_party, received_x, received_y));
-        },
+            shares_storage
+                .lock()
+                .await
+                .push((received_id_party, received_x, received_y));
+        }
         // Print the shares this server holds - For testing purposes
         ["SHOW_SHARES"] => {
             let shares_lock = shares_storage.lock().await;
-            println!("Shares of party {}: {:?}", current_id.to_string(), *shares_lock);
-        },
+            println!(
+                "Shares of party {}: {:?}",
+                current_id.to_string(),
+                *shares_lock
+            );
+        }
         _ => println!(""),
     }
 }
 
 // Async function to repeatedly try connecting to port 8081
-async fn connect_to_ports(targets: Vec<String>, local_port: String, connections: Arc<Mutex<HashMap<String, TcpStream>>>) {
-  for target_port in targets {
-      loop {
-          match TcpStream::connect(format!("127.0.0.1:{}", target_port)).await {
-              Ok(mut stream) => {
-                  stream
-                      .write(format!("HELLO {}", local_port).as_bytes())
-                      .await
-                      .unwrap();
-                  connections.lock().await.insert(target_port, stream);
-                  break;
-              }
-              Err(e) => {
-                  // eprintln!("Failed to connect to port {}: {}", target_port, e);
-                  time::sleep(Duration::from_secs(3)).await;
-              }
-          }
-          time::sleep(Duration::from_secs(1)).await; // Wait before retrying
-      }
-  }
+async fn connect_to_ports(
+    targets: Vec<String>,
+    local_port: String,
+    connections: Arc<Mutex<HashMap<String, TcpStream>>>,
+) {
+    for target_port in targets {
+        loop {
+            match TcpStream::connect(format!("127.0.0.1:{}", target_port)).await {
+                Ok(mut stream) => {
+                    stream
+                        .write(format!("HELLO {}", local_port).as_bytes())
+                        .await
+                        .unwrap();
+                    connections.lock().await.insert(target_port, stream);
+                    break;
+                }
+                Err(e) => {
+                    // eprintln!("Failed to connect to port {}: {}", target_port, e);
+                    time::sleep(Duration::from_secs(3)).await;
+                }
+            }
+            time::sleep(Duration::from_secs(1)).await; // Wait before retrying
+        }
+    }
 }
 
 fn parse_party(party_str: &str) -> (u64, u16) {
-  let parts: Vec<&str> = party_str.split(':').collect();
-  if parts.len() != 2 {
-      eprintln!("Invalid party format. Expected format: id:port");
-      std::process::exit(1);
-  }
-  let id = parts[0].parse::<u64>().expect("Invalid id");
-  let port = parts[1].parse::<u16>().expect("Invalid port number");
-  (id, port)
+    let parts: Vec<&str> = party_str.split(':').collect();
+    if parts.len() != 2 {
+        eprintln!("Invalid party format. Expected format: id:port");
+        std::process::exit(1);
+    }
+    let id = parts[0].parse::<u64>().expect("Invalid id");
+    let port = parts[1].parse::<u16>().expect("Invalid port number");
+    (id, port)
 }
 
 #[tokio::main]
 async fn main() {
-
     let args: Vec<String> = env::args().collect();
     // Input: current_id "id1:port1" "id2:port2" "id3:port3"
     if args.len() != 5 {
@@ -133,11 +176,13 @@ async fn main() {
     let party2 = parse_party(&args[3]);
     let party3 = parse_party(&args[4]);
     let parties = vec![party1, party2, party3];
-    let port = parties.iter()
+    let port = parties
+        .iter()
         .find(|(id, _)| id == current_id)
         .map(|(_, port)| *port)
         .expect("Current ID does not match any party");
-    let other_parties: Vec<(u64, u16)> = parties.into_iter()
+    let other_parties: Vec<(u64, u16)> = parties
+        .into_iter()
         .filter(|(id, _)| id != current_id)
         .collect();
 
@@ -146,9 +191,11 @@ async fn main() {
 
     // This is where received shares from other parties are stored
     let shares = Arc::new(Mutex::new(Vec::<(u64, Fq, Fq)>::new()));
-    
+
     // Start listening for incoming connection requests on localhost with given port
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).await.unwrap();
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", port))
+        .await
+        .unwrap();
     println!("Server running on port {}", port);
 
     let other_port_1 = other_parties[0];
@@ -162,24 +209,32 @@ async fn main() {
         vec![other_port_1.1.to_string(), other_port_2.1.to_string()],
         port.clone().to_string(),
         connections_clone,
-    )).await.unwrap();
+    ))
+    .await
+    .unwrap();
 
     // Print the connections after connecting with parties
     println!("Established connections: {:?}", connections);
-    
+
+    let clonned_conn_iter = Arc::clone(&connections);
+
     loop {
         // Waits for incoming connection attempt, stores established connection to the client
         let (incoming_connection, _) = listener.accept().await.unwrap();
 
         let shares_clone = shares.clone();
+        let out_connections = Arc::clone(&connections);
         // Create and start new async task, which is the MPC party
         tokio::spawn(async move {
             mpc_party(
-              incoming_connection, 
-              secret, 
-              current_party_id,
-              [other_port_1.0, other_port_2.0],
-              shares_clone).await;
+                incoming_connection,
+                out_connections,
+                secret,
+                current_party_id,
+                [other_port_1.0, other_port_2.0],
+                shares_clone,
+            )
+            .await;
         });
     }
 }
--- src/shamir_secret_sharing.rs
+++ src/shamir_secret_sharing.rs
@@ -1,6 +1,6 @@
-use ark_poly::{univariate::DensePolynomial, DenseUVPolynomial};
 use ark_ff::fields::{Fp64, MontBackend, MontConfig};
 use ark_poly::Polynomial;
+use ark_poly::{univariate::DensePolynomial, DenseUVPolynomial};
 use rand::Rng;
 
 const MODULUS: u64 = 127;
@@ -12,39 +12,39 @@ pub struct FqConfig;
 pub type Fq = Fp64<MontBackend<FqConfig, 1>>;
 
 pub fn generate_secret() -> Fq {
-  let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
-  let r: u64 = rng.gen();
-  let secret = r % MODULUS;
+    let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
+    let r: u64 = rng.gen();
+    let secret = r % MODULUS;
 
-  Fq::from(secret)
+    Fq::from(secret)
 }
 
 // Obtain n shares for a secret in Zq
 // The shares are obtained by creating a polynomial with degree t, where t < n/2
 // returns n shares
-pub fn get_shares_secret(secret: Fq, inputs: Vec<u64>, t:usize) -> Vec<(Fq, Fq)> {
-  // 1. Generate polynomial of deg t
-  let p = create_pol(secret, t);
+pub fn get_shares_secret(secret: Fq, inputs: Vec<u64>, t: usize) -> Vec<(Fq, Fq)> {
+    // 1. Generate polynomial of deg t
+    let p = create_pol(secret, t);
 
-  // 2. Evaluate polynomial at n points (1,..,n)
-  let mut evals = Vec::new();
-  for i in inputs {
-    let x = Fq::from(i);
-    let pi = p.evaluate(&x);
-    evals.push((x, pi));
-  }
+    // 2. Evaluate polynomial at n points (1,..,n)
+    let mut evals = Vec::new();
+    for i in inputs {
+        let x = Fq::from(i);
+        let pi = p.evaluate(&x);
+        evals.push((x, pi));
+    }
 
-  evals
+    evals
 }
 
 // Returns a polynomial s + a_1x + a_2x^2 + .. + a_tx^t
 fn create_pol(s: Fq, t: usize) -> DensePolynomial<Fq> {
-  let mut rng = ark_std::test_rng();
-  // Generate a random polynomial of degree t
-  let mut poly = DensePolynomial::rand(t, &mut rng);
+    let mut rng = ark_std::test_rng();
+    // Generate a random polynomial of degree t
+    let mut poly = DensePolynomial::rand(t, &mut rng);
 
-  // Replace the constant term by the secret
-  poly.coeffs[0] = s;
+    // Replace the constant term by the secret
+    poly.coeffs[0] = s;
 
-  poly
+    poly
 }
